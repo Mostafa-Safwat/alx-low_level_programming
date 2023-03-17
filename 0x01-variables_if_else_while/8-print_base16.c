0738cf956f8a8ec8d7d7c9e1be8af1f9fd1ddc14
Mostafa <mostafasafwat404@gmail.com>
2023-03-17 10:27:13 -0700

    hot fix

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -5,18 +5,18 @@
  *
  * Return: Always 0 (Success)
  */
-int main() {
+int main()
+{
     char ch;
 
-    for (ch = '0'; ch <= '9'; ch++) {
+    for (ch = '0'; ch <= '9'; ch++)
+    {
         putchar(ch);
     }
-
-    for (ch = 'a'; ch <= 'f'; ch++) {
+    for (ch = 'a'; ch <= 'f'; ch++)
+    {
         putchar(ch);
     }
-
     putchar('\n');
-
     return 0;
 }
