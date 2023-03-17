cefca27ff7dfa0ae754deaa11caf5be692e48e57
Mostafa <mostafasafwat404@gmail.com>
2023-03-17 10:39:19 -0700

    betty

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,18 +8,14 @@
 int main(void)
 {
 char ch;
-
-for (ch = '0'; ch <= '9'; ch++)
+for (ch='0';ch<='9';ch++)
 {
-    putchar(ch);
+putchar(ch);
 }
-
-for (ch = 'a'; ch <= 'f'; ch++)
+for (ch='a';ch<='f';ch++)
 {
-    putchar(ch);
+putchar(ch);
 }
-
 putchar('\n');
-
 return (0);
 }
