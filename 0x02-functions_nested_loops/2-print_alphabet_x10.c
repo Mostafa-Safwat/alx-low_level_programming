d009e3e140cd83fe97df23eb377616d3b105d143
Mostafa <mostafasafwat404@gmail.com>
2023-03-21 03:50:12 -0700

    s

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+/**
+ * print_alphabet_x10 - prints all letters in lowercase 10 times.
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+char alph[26] = "abcdefghijklmnopqrstuvwxyz";
+int i, j;
+for (j = 0; j < 10; j++){
+for (i = 0; i < 26; i++)
+{
+_putchar(alph[i]);
+}
+_putchar('\n');
+}
+}
