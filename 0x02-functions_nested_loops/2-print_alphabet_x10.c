9749d0c1e82beca5cb78676444d9aaf19e5f0f4f
Mostafa <mostafasafwat404@gmail.com>
2023-03-21 03:56:10 -0700

    a

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,7 +8,8 @@ void print_alphabet_x10(void)
 {
 char alph[26] = "abcdefghijklmnopqrstuvwxyz";
 int i, j;
-for (j = 0; j < 10; j++){
+for (j = 0; j < 10; j++)
+{
 for (i = 0; i < 26; i++)
 {
 _putchar(alph[i]);
