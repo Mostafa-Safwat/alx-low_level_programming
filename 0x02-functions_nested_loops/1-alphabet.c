818b5c7a450287963048eed7edb806058e6637f2
Mostafa <mostafasafwat404@gmail.com>
2023-03-21 03:35:03 -0700

    new file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - prints all letters in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+char alph[26] = "abcdefghijklmnopqrstuvwxyz";
+int i;
+
+for (i = 0; i < 26; i++)
+{
+_putchar(alph[i]);
+}
+_putchar('\n');
+return (0);
+}
