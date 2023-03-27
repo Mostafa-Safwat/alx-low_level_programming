c23072baca1f3f21e2dc95de338b83eb9ad4feb0
Mostafa <mostafasafwat404@gmail.com>
2023-03-27 07:08:21 -0700

    w

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+/**
+ * rev_string - reverses a string
+ * @s: the string to reverse
+ */
+void rev_string(char *s)
+{
+int len = 0;
+char *begin, *end, tmp;
+
+while (*(s + len))
+{
+len++;
+}
+
+begin = s;
+end = s + len - 1;
+
+while (begin < end)
+{
+tmp = *begin;
+*begin = *end;
+*end = tmp;
+begin++;
+end--;
+}
+}
