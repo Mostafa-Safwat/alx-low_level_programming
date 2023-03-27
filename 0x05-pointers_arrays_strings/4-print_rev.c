6b13fe111576feb6f7781290d118ace9c9b72084
Mostafa <mostafasafwat404@gmail.com>
2023-03-27 07:01:49 -0700

    o

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,14 +5,17 @@
  */
 void print_rev(char *s)
 {
-int len = 0;
+int i, len = 0;
 
-while (s[len] != '\0')
+while (*(s + len) != '\0')
+{
 len++;
+}
 
-int i;
 for (i = len - 1; i >= 0; i--)
-_putchar(s[i]);
+{
+_putchar(*(s + i));
+}
 
 _putchar('\n');
 }
