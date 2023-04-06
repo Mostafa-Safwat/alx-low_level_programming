c5b690c5e5f25ab2ce71c73c74a434fddf3cd8ac
Mostafa-Safwat <mostafasafwat404@gmail.com>
2023-04-06 21:27:30 +0200

    send help

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,45 +1,45 @@
 #include "main.h"
+
 /**
- * _sqrt_recursion - returns the natural square root of a number
- * @n: the number to find the square root of
+ * sqrt_helper - returns the natural square root of a number.
+ * @n: input number.
+ * @i: number to check if it is a natural square root of n.
  *
- * Return: the square root of n, or -1 if n does not have a natural square root
+ * Return: the natural square root of n, -1 if n does not have
+ * a natural square root.
  */
-int sqrt_2(int n, int i);
+int sqrt_helper(int n, int i);
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number.
+ * @n: input number.
+ *
+ * Return: the natural square root of n, -1 if n does not have
+ * a natural square root.
+ */
 int _sqrt_recursion(int n)
 {
-    if (n < 0)
-    {
-        return (-1);
-    }
-    else if (n == 0 || n == 1)
-    {
-        return (n);
-    }
-    else
-    {
-        return (sqrt_2(n, 1));
-    }
+	return (sqrt_helper(n, 1));
 }
+
 /**
- * sqrt - helper function to recursively calculate square root
- * @n: the number to find the square root of
- * @i: the current guess for the square root
+ * sqrt_helper - returns the natural square root of a number.
+ * @n: input number.
+ * @i: number to check if it is a natural square root of n.
  *
- * Return: the square root of n, or -1 if n does not have a natural square root
+ * Return: the natural square root of n, -1 if n does not have
+ * a natural square root.
  */
-double sqrt_2(int n, int i)
+
+int sqrt_helper(int n, int i)
 {
-    if (i * i == n)
-    {
-        return (i);
-    }
-    else if (i * i > n)
-    {
-        return (-1);
-    }
-    else
-    {
-        return (sqrt_2(n, i + 1));
-    }
+	if (n == 0)
+		return (0);
+	if (n == 1)
+		return (1);
+	if (i * i == n)
+		return (i);
+	if (i * i > n)
+		return (-1);
+	return (sqrt_helper(n, i + 1));
 }
