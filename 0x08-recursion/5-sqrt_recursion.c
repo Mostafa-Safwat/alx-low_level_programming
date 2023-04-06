ab6faf3fc986b7edb2d09921070272dbf27ce89b
Mostafa-Safwat <mostafasafwat404@gmail.com>
2023-04-06 21:28:44 +0200

    small change

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,14 +9,6 @@
  * a natural square root.
  */
 int sqrt_helper(int n, int i);
-
-/**
- * _sqrt_recursion - returns the natural square root of a number.
- * @n: input number.
- *
- * Return: the natural square root of n, -1 if n does not have
- * a natural square root.
- */
 int _sqrt_recursion(int n)
 {
 	return (sqrt_helper(n, 1));
