87b2d5a3de4a295d90556b19f71d7f36e2d6a600
Mostafa-Safwat <mostafasafwat404@gmail.com>
2023-04-06 21:41:14 +0200

    hope 2

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,4 +1,7 @@
-include "main.h"
+#include "main.h"
+
+int check_divisibility(int n, int i);
+
 /**
  * check_divisibility - Checks if a number is divisible by another number
  * @n: The number to check
@@ -6,24 +9,23 @@ include "main.h"
  *
  * Return: 1 if n is divisible by divisor, 0 otherwise
  */
-int check_divisibility(int n, int divisor)
+int is_prime_number(int n)
 {
-    if (n % divisor == 0)
-        return (1);
-    if (divisor * divisor > n)
-        return (0);
-    return (check_divisibility(n, divisor + 1));
+	if (n <= 1)
+		return (0);
+	return (check_divisibility(n, n - 1));
 }
-
 /**
  * is_prime_number - Checks if a number is prime
  * @n: The number to check
  *
  * Return: 1 if n is prime, 0 otherwise
  */
-int is_prime_number(int n)
+int check_divisibility(int n, int i)
 {
-    if (n <= 1)
-        return (0);
-    return (!check_divisibility(n, 2));
+	if (i == 1)
+		return (1);
+	if (n % i == 0 && i > 0)
+		return (0);
+	return (check_divisibility(n, i - 1));
 }
