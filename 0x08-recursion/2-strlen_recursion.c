11a78b6cf3c66cd15b643c042964d2dab99cd4cc
Mostafa-Safwat <mostafasafwat404@gmail.com>
2023-04-06 21:07:28 +0200

    hot fix

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -7,9 +7,11 @@
  */
 int _strlen_recursion(char *s)
 {
-if (*s == '\0')
+int len = 0;
+if (*s)
 {
-return (0);
+len++;
+len += _strlen_recursion(s + 1);
 }
-return _strlen_recursion((s + 1) + 1);
+return (len);
 }
