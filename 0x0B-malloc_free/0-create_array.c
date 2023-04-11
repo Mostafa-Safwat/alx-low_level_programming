2a917cc199944a89472c78f9dc0cac0e11eb9b15
Mostafa-Safwat <mostafasafwat404@gmail.com>
2023-04-11 15:10:27 +0200

    hot fix

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,14 +13,17 @@ if (size == 0)
 {
 return (NULL);
 }
-char *arr = (char *)malloc(size * sizeof(char));
+char *arr;
+arr = (char *)malloc(size * sizeof(char));
 if (arr == NULL)
 {
 return (NULL);
 }
-for (unsigned int i = 0; i < size; i++)
+unsigned int i;
+for (i = 0; i < size; i++)
 {
 arr[i] = c;
 }
 return (arr);
 }
+
