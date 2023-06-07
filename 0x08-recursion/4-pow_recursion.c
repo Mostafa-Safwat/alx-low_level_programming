fd001f4af065faad0f3fe4cdebacfbbfd4591ded
Mostafa-Safwat <mostafasafwat404@gmail.com>
2023-06-07 10:10:56 +0300

    new files

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+/**
+ * _pow_recursion - computes the value of x raised to the power of y
+ * @x: the base
+ * @y: the exponent
+ *
+ * Return: the result of x raised to the power of y
+ *         -1 if y is negative
+ */
+int _pow_recursion(int x, int y)
+{
+if (y == 0)
+return (1);
+if (y < 0)
+return (-1);
+return (x * _pow_recursion(x, y - 1));
+}
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * check_divisibility - Checks if a number is divisible by another number
+ * @n: The number to check
+ * @i: The divisor
+ *
+ * Return: 1 if n is divisible by divisor, 0 otherwise
+ */
+int check_divisibility(int n, int i);
+/**
+ * is_prime_number - Checks if a number is prime
+ * @n: The number to check
+ *
+ * Return: 1 if n is prime, 0 otherwise
+ */
+int is_prime_number(int n)
+{
+if (n <= 1)
+return (0);
+return (check_divisibility(n, n - 1));
+}
+/**
+ * check_divisibility - Checks if a number is divisible by another number
+ * @n: The number to check
+ * @i: The divisor
+ *
+ * Return: 1 if n is divisible by divisor, 0 otherwise
+ */
+int check_divisibility(int n, int i)
+{
+if (i == 1)
+return (1);
+if (n % i == 0 && i > 0)
+return (0);
+return (check_divisibility(n, i - 1));
+}
